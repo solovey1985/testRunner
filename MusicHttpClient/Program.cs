e9543849de774ef3530fad0a0dfaab962533c73c
Andrii Solovienko <andrii_solovienko@epam.com>
2017-05-18 12:43:33 +0300

    initial commit

---
+++ MusicHttpClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MusicHttpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aw =  GetRequest();
+            while (!aw.IsCompleted)
+            {
+                Thread.Sleep(500);
+            }
+
+            Console.Write(aw.Result.ReadAsStringAsync().Result);
+            Console.ReadKey();
+        }
+
+
+        public static async Task<HttpContent> GetRequest()
+        {
+            HttpClient client = new HttpClient();
+            var result = await client.SendAsync(new HttpRequestMessage()
+            { RequestUri = new Uri(@"https://www.ukr.net/") }
+            );
+            return result.Content;
+        }
+        
+    }
+}
