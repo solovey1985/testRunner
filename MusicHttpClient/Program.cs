4513e8893972f4a1977f69844ddbd197eaa7ae61
Andrew <solovey1985@ukr.net>
2017-05-19 18:27:25 +0300

    Additional logging

--- MusicHttpClient/Program.cs
+++ MusicHttpClient/Program.cs
@@ -18,7 +18,7 @@ namespace MusicHttpClient
             {
                 Thread.Sleep(500);
             }
-
+            Console.WriteLine("Output of results");
             Console.Write(aw.Result.ReadAsStringAsync().Result);
             Console.ReadKey();
         }
