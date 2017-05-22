ecdb8ea1c83c08c0ef43e2da0402441afb63b81f
Andrew <solovey1985@ukr.net>
2017-05-22 11:03:56 +0300

    Update Program.cs

--- MusicHttpClient/Program.cs
+++ MusicHttpClient/Program.cs
@@ -19,6 +19,7 @@ namespace MusicHttpClient
                 Thread.Sleep(500);
             }
             Console.WriteLine("Output of another results changed");
+            Console.WriteLine("Updated ++");
             Console.ReadKey();
         }
 
